9627ff1208905ffc433ff445054ea79f57a767a3
deshawashington <washind14@icloud.com>
2022-05-27 15:48:24 -0600

    commit

---
+++ Form.java
@@ -0,0 +1,8 @@
+public class Form {
+    public static void main(String[] args) {
+        char gender = 'F';
+        System.out.println("Gender: " + gender);
+        String name = "Sam";
+        System.out.println("Name: " + name);
+    }
+}
